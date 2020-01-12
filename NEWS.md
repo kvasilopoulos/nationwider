b262a90887a59a5333d76569d24901b479090c4b
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2020-01-12 20:54:22 +0000

    added timeout and demoted errors and warning in ntwd_get to messages

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# nationwider 1.1.1
+
+* Fixed timeout error in tests (basically skipped_on_cran)
+
 # nationwider 1.1.0
 
 * Added a `NEWS.md` file to track changes to the package.
--- R/tf.R
+++ R/tf.R
@@ -1,15 +1,19 @@
-#' @importFrom httr GET write_disk
+#' @importFrom httr GET write_disk timeout
 #' @importFrom rvest html_nodes html_attrs
 #' @importFrom xml2 read_html
-ntwd_tf <- function(file = NULL, regexp = NULL, access_info = TRUE) {
+ntwd_tf <- function(file = NULL, regexp = NULL, access_info = FALSE) {
+
+  dl_url <- "https://www.nationwide.co.uk/about/house-price-index/download-data"
+  con_url <- GET(dl_url, timeout(10))
+
   urls <-
-    xml2::read_html(
-      "https://www.nationwide.co.uk/about/house-price-index/download-data") %>%
+    xml2::read_html(con_url) %>%
     rvest::html_nodes(".concertina") %>%
     rvest::html_nodes("a") %>%
     rvest::html_attr("href") %>%
     grep(".xls", ., value = TRUE) %>%
     paste0("https://www.nationwide.co.uk", .)
+
   if (is.null(file) && is.null(regexp)) {
     return(urls)
   }
@@ -25,7 +29,7 @@ ntwd_tf <- function(file = NULL, regexp = NULL, access_info = TRUE) {
   if (interactive() && access_info)
     message("Accessing ", url)
   tf <- tempfile(fileext = ".xls")
-  httr::GET(url, write_disk(tf))
+  GET(url, write_disk(tf), timeout(10))
   structure(tf, source = url, class = "access_url")
 }
 
